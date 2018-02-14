14804fdb895cc46dcef60db734d91815e7b5248c
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-14 15:36:14 +0100

    Exo 1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,46 @@
 class ReviewsController < ApplicationController
   def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def edit
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+    # # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = @restaurant
+    # @review.save
+    # redirect_to restaurant_path(@restaurant)
+    p @review
+
+    respond_to do |format|
+      if @review.save
+        p "coucoucoucocu"
+        format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        p "totototototototo"
+        p @review.errors
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def index
   end
 
   def update
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: 'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 26 13 76 65',
+    category:     'italian'
+  },
+  {
+    name:         'Woko',
+    address:      '7 Boundary St, Confluence E2 7JE',
+    phone_number: '04 26 13 76 65',
+    category:     'japanese'
+  },
+  {
+    name:         'Burger',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 26 13 76 65',
+    category:     'japanese'
+  },
+  {
+    name:         'Paul',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '04 26 13 76 65',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
